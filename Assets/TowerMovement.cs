fb27abf45397d24c10958497d3259f1a755dbe4b
Og_Vobla <gggg26705@gmail.com>
2023-03-19 16:50:52 +0300

    Initial commit

---
+++ Assets/TowerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TowerMovement : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+
+	}
+}
